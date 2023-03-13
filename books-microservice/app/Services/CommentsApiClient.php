084ed3a24e8b6bce0602b456f15508c33c26c90d
twent <twentprod@gmail.com>
2023-03-13 19:14:37 +0300

    🛠️ Refactoring

--- books-microservice/app/Services/CommentsApiClient.php
+++ books-microservice/app/Services/CommentsApiClient.php
@@ -4,31 +4,22 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
 
 final class CommentsApiClient
 {
-    public function __construct(
-        private readonly Client $httpClient = new Client()
-    ) {
-    }
-
     /**
      * @throws GuzzleException
      */
     public function get(string $entityType, int $entityId): Collection
     {
-        $response = $this->httpClient->get('http://comments-app/api/v1/comments', [
-            'query' => [
-                'entityType' => $entityType,
-                'entityId' => $entityId,
-            ],
+        $response = Http::get('http://comments-app/api/v1/comments', [
+            'entityType' => $entityType,
+            'entityId' => $entityId,
         ]);
 
-        return collect(
-            json_decode($response->getBody()->getContents(), true)
-        );
+        return collect($response->json());
     }
 }
